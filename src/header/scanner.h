2adc155751873fb227c1cb8dcdedc086f7b633d0
mato0036 <mato0036@gmail.com>
2018-11-11 15:21:02 +0100

    Added coments

--- src/header/scanner.h
+++ src/header/scanner.h
@@ -6,35 +6,35 @@
 
 
 typedef enum automataState{
-    STATE_BEGIN,				// DONE
-    STATE_INT,					// DONE
-	STATE_INT_DOT,				// DONE
-	STATE_INT_E,				// DONE
-	STATE_INT_EXPO,				// DONE
-    STATE_FLOAT,				// DONE
-    STATE_FLOAT_E,				// DONE
-	STATE_FLOAT_EXPO_PLUS,		// DONE
-	STATE_FLOAT_EXPO,			// DONE
-    STATE_ID,					// DONE
-    STATE_LCOM,					// DONE
-    STATE_BCOM,					// DONE
-	STATE_BCOM_EQUALS,			// DONE
-	STATE_BCOM_B,				// DONE
-	STATE_BCOM_E,				// DONE
-	STATE_BCOM_G,				// DONE
-	STATE_BCOM_I,				// DONE
-	STATE_BCOM_N,				// DONE
-	STATE_BCOM_COM,				// DONE
-	STATE_BCOM_COM_EOL,			// DONE
-	STATE_BCOM_COM_EQUALS,		// DONE
-	STATE_BCOM_COM_E,			// DONE
-	STATE_BCOM_COM_N,			// DONE
-	STATE_BCOM_COM_D,			// DONE
-	STATE_BCOM_COM_2,			// DONE
-	STATE_ASSIGN,				// DONE
-	STATE_LESS,					// DONE
-	STATE_GREAT,				// DONE
-	STATE_NEG,					// DONE
+    STATE_BEGIN,
+    STATE_INT,
+	STATE_INT_DOT,
+	STATE_INT_E,
+	STATE_INT_EXPO,
+    STATE_FLOAT,
+    STATE_FLOAT_E,
+	STATE_FLOAT_EXPO_PLUS,
+	STATE_FLOAT_EXPO,
+    STATE_ID,
+    STATE_LCOM,
+    STATE_BCOM,
+	STATE_BCOM_EQUALS,
+	STATE_BCOM_B,
+	STATE_BCOM_E,
+	STATE_BCOM_G,
+	STATE_BCOM_I,
+	STATE_BCOM_N,
+	STATE_BCOM_COM,
+	STATE_BCOM_COM_EOL,
+	STATE_BCOM_COM_EQUALS,
+	STATE_BCOM_COM_E,
+	STATE_BCOM_COM_N,
+	STATE_BCOM_COM_D,
+	STATE_BCOM_COM_2,
+	STATE_ASSIGN,
+	STATE_LESS,
+	STATE_GREAT,
+	STATE_NEG,
 
 } automataState;
 
@@ -53,21 +53,47 @@ typedef enum tokenType{
     TYPE_NEG_EQUAL,     //  !=
     TYPE_L_BRE,         //  (
     TYPE_R_BRE,         //  )
-    TYPE_L_COM,
-    TYPE_B_COM,
-    TYPE_INT,
-	TYPE_INT_EXPO,
-    TYPE_FLOAT,
-	TYPE_FLOAT_EXPO,
-	TYPE_ID,
-	TYPE_FUNC_ID,
-    TYPE_EOF,
-	TYPE_ERROR,
+    TYPE_L_COM,			//  Line Comment
+    TYPE_B_COM,			//	Block Comment
+    TYPE_INT,			//  Integer
+	TYPE_INT_EXPO,		//	Integer with exponential
+    TYPE_FLOAT,			//	Float (decimal number)
+	TYPE_FLOAT_EXPO,	//	Float with exponential (decimal number)
+	TYPE_ID,			//	ID of variable or function
+	TYPE_FUNC_ID,		//	ID of function ( ? or ! at end )
+    TYPE_EOF,			// 	End of file
+	TYPE_ERROR,			//	Some Error
 
 } tokenType;
 
+
+/**
+ *	Function allocate memory for buffer, where it adds first char from reading text
+ *
+ *	@brief	Function for initializing buffer
+ *
+ *	@param	*buffer	Pointer to ...
+ *			c		Char to be inserted to buffer
+ */
 void firstChar(char *buffer, char c);
 
+
+/**
+ *	Function reallocate memory for bigger size and insert other char from reading text
+ *
+ *	@brief	Insert another char to buffer
+ *
+ *	@param	*buffer Pointer to ...
+ *			c		Char to be inserted to buffer
+ *			length	New length of buffer
+ */
 void addChar(char *buffer, char c, unsigned length);
 
+
+/**
+ *	Function read text and parse it to different types
+ *
+ *	@param	*buffer	Pointer to ...
+ *	@return	Number from enum tokenType.
+ */
 int scanner(char *buffer);
