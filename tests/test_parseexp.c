dcf2dca1fd21789d118103a3126d772d935e7c11
Petku <nikopatrik17@gmail.com>
2018-11-25 00:06:32 +0100

    Edited test so it handle functional garbage collector.

--- tests/test_parseexp.c
+++ tests/test_parseexp.c
@@ -3,9 +3,11 @@
 
 #include "../src/header/scanner.h"
 #include "../src/header/parseexp.h"
+#include "../src/header/garbagecollector.h"
 
 int main()
 {
+    garbage_collector_init();
     myQueueInit();
     if(expression_parse( TYPE_KEYWORD, "then"))
         puts("Syntax correct!!!");
@@ -13,5 +15,6 @@ int main()
         puts("Syntax not correct!!!");
 
     myQueueFree();
-    return 0;
+    gb_release_resources();
+        return 0;
 }
