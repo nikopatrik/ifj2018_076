4b21f412a350010c0aa51ea0db0c0063f1ccfb9e
PetoH <peterhornak77@gmail.com>
2018-11-29 23:54:17 +0100

    Zabudnuty main pridany

--- src/main.c
+++ src/main.c
@@ -7,19 +7,22 @@
 #include "header/parser.h"
 #include "header/symtable.h"
 #include "header/scanner.h"
+#include "header/generate.h"
 
 int main(){
     
-    unsigned i = 10;
+    int i = -1;
     htab_setup();
     myQueueInit();
+    myStackInit();
     bool corr = true;
     corr =  parse();
-
-    if(corr == true)
+    //myQueueFree();
+    gb_release_resources();
+    if(corr == true){
         printf("correct!\n");
+        return 0;
+    }
     else
         printf("wrong!\n");
-
-    myQueueFree();
 }
